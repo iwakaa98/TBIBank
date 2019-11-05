3671ed877e5962bf7fcfa193469a65ed94d659e8
wutow_i <wutow_i@abv.bg>
2019-11-05 11:01:43 +0200

    login.js added

---
+++ TBIBankApp/wwwroot/js/login.js
@@ -0,0 +1,24 @@
+﻿
+$('.login100-form-btn').click(
+    function (e) {
+        let username = $('#inputUserName').val();
+        let password = $('#inputPassword').val();
+        console.log(username);
+        if (!username) {
+            $('#inputUserName').addClass('border', 'solid red 2px');
+            $('#focus-UserName').text('Please enter username');
+            e.preventDefault();
+        }
+        if (username) {
+            $('#focus-UserName').text('');
+        }
+        if (!password) {
+            $('#inputPassword').addClass('border', 'solid red 2px');
+            $('#focus-Password').text('Please enter password');
+            e.preventDefault();
+        }
+        if (password) {
+            $('#focus-Password').text('');
+        }
+
+    })
