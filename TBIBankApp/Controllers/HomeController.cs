0bde534d820b8608bda2d632c6ef8a2a7c6703f9
wutow_i <wutow_i@abv.bg>
2019-11-11 01:40:44 +0200

    testing modals

--- TBIBankApp/Controllers/HomeController.cs
+++ TBIBankApp/Controllers/HomeController.cs
@@ -50,6 +50,10 @@ namespace TBIBankApp.Controllers
                 {
                     
                     var user = userManager.FindByNameAsync(Input.UserName).Result;
+                    if(user.LastLogIn.Year==1)
+                    {
+
+                    }
                     //if (user.LastLogIn.Year == 1)
                     //{
                     //    return Redirect("ChangePassword");
