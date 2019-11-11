40ae6b046de3848ad5549bf975ba935e8a81e602
wutow_i <wutow_i@abv.bg>
2019-11-11 16:55:45 +0200

    Modals added finally

--- TBIBankApp/wwwroot/js/modalButtons.js
+++ TBIBankApp/wwwroot/js/modalButtons.js
@@ -1,8 +1,14 @@
-﻿$(document).ready(function () {
+﻿let butTestDisable;
+$(document).ready(function () {
     $('#example').DataTable();
 });
 function ModalTest(id) {
     $(`.${id}`).modal('show');
+    butTestDisable = document.getElementById(id);
+    setTimeout('Disable()', 0,01);
+}
+function Disable() {
+    butTestDisable.setAttribute("disabled", true);
 }
 
 function SetInvalid(value) {
@@ -31,6 +37,58 @@ function SetNew(value) {
     data = {
         id: value,
         status: 'New'
+    };
+    $.ajax(
+        {
+            type: "Get",
+            url: "ChangeStatus",
+            data: data,
+            success: function () {
+            }
+        })
+}
+
+function SetClosed(value) {
+    console.log(value)
+    let but = document.getElementById(value + '+classID');
+    but.remove();
+    data = {
+        id: value,
+        status: 'Closed'
+    };
+    $.ajax(
+        {
+            type: "Get",
+            url: "ChangeStatus",
+            data: data,
+            success: function () {
+            }
+        })
+}
+function SetNotReviewed(value) {
+    console.log(value)
+    let but = document.getElementById(value + '+classID');
+    but.remove();
+    data = {
+        id: value,
+        status: 'NotReviewed'
+    };
+    $.ajax(
+        {
+            type: "Get",
+            url: "ChangeStatus",
+            data: data,
+            success: function () {
+            }
+        })
+}
+function SetOpen(value) {
+    console.log(value)
+    let but = document.getElementById(value + '+classID');
+    but.remove();
+    data = {
+        id: value,
+        status: 'Open'
     };
     $.ajax(
         {
