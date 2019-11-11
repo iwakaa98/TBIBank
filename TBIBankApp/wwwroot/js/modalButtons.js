a547c2348729b53b7eafeaa0bba15de2d117f54d
wutow_i <wutow_i@abv.bg>
2019-11-11 05:26:38 +0200

    Added some Modals

---
+++ TBIBankApp/wwwroot/js/modalButtons.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $('#example').DataTable();
+});
+function ModalTest(id) {
+    $(`.${id}`).modal('show');
+}
+
+function SetInvalid(value) {
+    let but = document.getElementById(value+'+classID');
+    console.log(but);
+    but.remove();
+    data = {
+        id: value,
+        status: 'InvalidApplication'
+    };
+    $.ajax(
+        {
+            type: "Get",
+            url: "ChangeStatus",
+            data: data,
+            success: function () {
+            }
+        })
+};
+
+
+function SetNew(value) {
+    console.log(value)
+    let but = document.getElementById(value+'+classID');
+    but.remove();
+    data = {
+        id: value,
+        status: 'New'
+    };
+    $.ajax(
+        {
+            type: "Get",
+            url: "ChangeStatus",
+            data: data,
+            success: function () {
+            }
+        })
+}
